2c0dea4c68ef703e2813f416ee5540ce7119cdfd
Michael <michael.hrabe@gmail.com>
2023-03-12 21:43:56 +0100

    day24

--- aoc2022/src/aoc/day24/task.cpp
+++ aoc2022/src/aoc/day24/task.cpp
@@ -66,6 +66,18 @@ struct grid_element {
     };
 };
 
+struct grid_element_state
+{
+    grid_element g_element;
+    int time;
+
+    grid_element_state(grid_element g_element, int time){
+        this->g_element = g_element;
+        this->time = time;
+    }
+};
+
+
 class grid_layout {
     public:
         const static int ROW = 22;
@@ -83,10 +95,10 @@ class grid_layout {
 };
 
 std::shared_ptr<grid_layout> parse_input();
-
-int main(){
-    return 0;
-}
+int bfs(std::shared_ptr<grid_layout> grid_layout_ptr, grid_element player, std::vector<position>& directions);
+bool can_move_on_grid(position& el_position, position& dir, std::shared_ptr<grid_layout> grid_layout_ptr);
+void simulate_blizzards(std::shared_ptr<grid_layout> grid_layout_ptr);
+int get_result();
 
 std::shared_ptr<grid_layout> parse_input(){
     auto input = std::ifstream("/home/michael/git/adventofcode/aoc2022/src/aoc/day24/in.txt");
@@ -149,19 +161,37 @@ std::shared_ptr<grid_layout> parse_input(){
 }
 
 int bfs(std::shared_ptr<grid_layout> grid_layout_ptr, grid_element player, std::vector<position>& directions){
-    std::queue<grid_element> q;
-    q.push(player);
+    std::queue<grid_element_state> q;
+
+    grid_element_state element = grid_element_state(player, 1);
+    q.push(element);
+    int time = 0;
 
     while(!q.empty()){
         auto& current = q.front();
         q.pop();
-
-        for(auto& dir : directions){
-            grid_element ge = grid_element(current.type, current.pos + dir, dir);
+        if(time != current.time){
+            time++;
+            simulate_blizzards(grid_layout_ptr);
         }
 
+        for(auto& dir : directions){
+            grid_element ge = grid_element(current.g_element.type, current.g_element.pos + dir, dir);
+            grid_element_state ges = grid_element_state(ge, current.time + 1);
+            if(can_move_on_grid(ge.pos, ge.dir, grid_layout_ptr)){
+                q.push(ges);
+            }
+        }
     }
-    return 0;
+    return time;
+}
+
+bool can_move_on_grid(position& el_position, position& dir, std::shared_ptr<grid_layout> grid_layout_ptr){
+    position pos_res = el_position + dir;
+    if((*(*grid_layout_ptr).grid)[pos_res.y][pos_res.x] == 0){
+        return true;
+    }
+    return false;
 }
 
 void simulate_blizzards(std::shared_ptr<grid_layout> grid_layout_ptr){
@@ -204,6 +234,7 @@ int get_result(){
     direction dir = direction();
     std::vector<position> directions = {dir.right, dir.left, dir.up, dir.down, dir.none};
 
+
     // std::cout << (*grid_layout_ptr->grid.get())[1][1] << std::endl;
-    return 0;
+    return bfs(grid_layout_ptr, player, directions);
 }
