3a4119cc415e296cb5b90b6a1d174de9d1ed3164
Michael <michael.hrabe@gmail.com>
2023-10-21 18:42:59 +0200

    t6 different methods

--- aoc2021/src/t6/task.cpp
+++ aoc2021/src/t6/task.cpp
@@ -1,8 +1,22 @@
 // Model growth of Lantern Fish
+// first part: how many fish after 80 days
+// second part: how many fish after 256 days
+
+// $ ./task 256
+// Using 2D memo array:1695929023803
+// 0.00010114 seconds
+// Using 1D memo array:1695929023803
+// 2.4143e-05 seconds
+// Using 1D memo array:1695929023803
+// 2.4489e-05 seconds
+// Using recurse:1695929023803
+// 114.802 seconds
+
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <array>
+#include <map>
 #include <sstream>
 #include <ctime>
 #include <filesystem>
@@ -44,14 +58,26 @@ long naive_recurse(int days, int number){
 long get_naive_recurse_result(std::vector<int>& start_lanternfish_numbers, int days = 80){
     
     long res = 0;
+    std::map<int, long> memo;
     for(const auto& num : start_lanternfish_numbers){
-        res = res + naive_recurse(days, num);
+        
+        long tmp;
+        if (memo.find( num ) != memo.end()){
+            tmp = memo[num];
+        }
+        else{
+            tmp = naive_recurse(days, num);
+            memo[num] = tmp;
+        }
+        
+        res += tmp;
     }
 
     std::cout << "Using recurse:" << res << std::endl;
     return res;
 }
 
+// Exponential time, // Exponential memory
 int get_naive_simulation_result(std::vector<int> inputs, int days = 80){
     for(int i = 0; i < days; i++){
         int k = inputs.size();
@@ -67,6 +93,7 @@ int get_naive_simulation_result(std::vector<int> inputs, int days = 80){
     return inputs.size();
 }
 
+// O(days) - time, O(days*cycle_count) - memory
 long get_result_using_2d(std::vector<int> & start_lanternfish_numbers, int days = 80){
 
     const int states_n = 9;
@@ -95,7 +122,7 @@ long get_result_using_2d(std::vector<int> & start_lanternfish_numbers, int days 
     return res;
 }
 
-
+// O(days) - time, O(days) - memory
 long get_result_using_1d(std::vector<int> & start_lanternfish_numbers, int days = 80){
 
     // shift array
@@ -115,6 +142,7 @@ long get_result_using_1d(std::vector<int> & start_lanternfish_numbers, int days 
     return res;
 }
 
+// O(days) - time, O(1) - memory
 long get_result_using_1d_memory_improved(std::vector<int> & start_lanternfish_numbers, int days = 80){
 
     long res = 0;
@@ -140,19 +168,18 @@ int main(int argc, char* argv[]){
 
     int days = std::stoi(argv[1]);
     const int naive_day_limit = 140;
+    const int naive_recurse_day_limit = 257;
     
     std::vector<int> numbers = read_test_input();
     auto res_2d = make_decorator(get_result_using_2d)(numbers, days);
     auto res_1d = make_decorator(get_result_using_1d)(numbers, days);
     auto res_1d_impr = make_decorator(get_result_using_1d_memory_improved)(numbers, days);
-    
-    
+
     if (days < naive_day_limit){
         auto res_naive_iteration = make_decorator(get_naive_simulation_result)(numbers, days);
-        auto res_recurse = make_decorator(get_naive_recurse_result)(numbers, days);
     }
-    else {
-        std::cout << "Not using naive method for more than " << naive_day_limit << " days." << std::endl;
+    if (days < naive_recurse_day_limit){
+        auto res_recurse = make_decorator(get_naive_recurse_result)(numbers, days);
     }
     
     return 0;
