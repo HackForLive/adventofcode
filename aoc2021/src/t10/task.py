07e9f4a3062606e0f3feec48b85aea953c2f86f8
Michael <michael.hrabe@gmail.com>
2023-11-07 22:14:19 +0100

    t10

---
+++ aoc2021/src/t10/task.py
@@ -0,0 +1,167 @@
+import time
+import os
+import functools
+import pathlib
+from enum import Enum
+from dataclasses import dataclass
+
+from collections import deque
+from line_profiler import LineProfiler
+
+
+curr_dir = pathlib.Path(__file__).parent.resolve()
+input_file = os.path.join(curr_dir, 'test.txt')
+
+class BracketType(Enum):
+    """
+    Bracket type, e.g. ( - open and ) - close
+    """
+    CLOSE = 0
+    OPEN = 1
+
+
+class BracketName(Enum):
+    """
+    Bracket name, e.g. () - round
+    """
+    ROUND = 0
+    SQUARED = 1
+    CURLY = 2
+    ANGLED = 3
+
+
+@dataclass
+class Bracket:
+    """
+    Bracket data class
+    """
+    name: BracketName
+    type: BracketType
+    score: int
+
+
+def get_scoring_1():
+    return {
+        BracketName.ROUND: 3,
+        BracketName.SQUARED: 57,
+        BracketName.CURLY: 1197,
+        BracketName.ANGLED: 25137
+    }
+
+
+def get_scoring_2():
+    return {
+        BracketName.ROUND: 1,
+        BracketName.SQUARED: 2,
+        BracketName.CURLY: 3,
+        BracketName.ANGLED: 4
+    }
+
+
+def create_bracket(bracket: str, score_dic) -> Bracket:
+    bracket_obj = None
+    if bracket == '(':
+        bracket_obj = Bracket(name=BracketName.ROUND, type=BracketType.OPEN,
+                              score=score_dic[BracketName.ROUND])
+    elif bracket == ')':
+        bracket_obj = Bracket(name=BracketName.ROUND, type=BracketType.CLOSE,
+                              score=score_dic[BracketName.ROUND])
+    elif bracket == '[':
+        bracket_obj = Bracket(name=BracketName.SQUARED, type=BracketType.OPEN,
+                              score=score_dic[BracketName.SQUARED])
+    elif bracket == ']':
+        bracket_obj = Bracket(name=BracketName.SQUARED, type=BracketType.CLOSE,
+                              score=score_dic[BracketName.SQUARED])
+    elif bracket == '{':
+        bracket_obj = Bracket(name=BracketName.CURLY, type=BracketType.OPEN,
+                              score=score_dic[BracketName.CURLY])
+    elif bracket == '}':
+        bracket_obj = Bracket(name=BracketName.CURLY, type=BracketType.CLOSE,
+                              score=score_dic[BracketName.CURLY])
+    elif bracket == '<':
+        bracket_obj = Bracket(name=BracketName.ANGLED, type=BracketType.OPEN,
+                              score=score_dic[BracketName.ANGLED])
+    elif bracket == '>':
+        bracket_obj = Bracket(name=BracketName.ANGLED, type=BracketType.CLOSE,
+                              score=score_dic[BracketName.ANGLED])
+    else:
+        raise NotImplementedError(f"Bracket name: {bracket} is not implemented yet!")
+
+    return bracket_obj
+
+
+def timer_decorator(func):
+    functools.wraps(func)
+    def with_timer(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        t1 = time.time()
+        elapsed = t1 - t0
+
+        print(f"@timer: {func.__name__} took {elapsed:0.4f} seconds")
+        return result
+    return with_timer
+
+
+def corrupted_bracket_pricer(line: str) -> int:
+    if not line:
+        return 0
+    stack = deque()
+    for c in line:
+        bracket = create_bracket(c, score_dic=get_scoring_1())
+        if bracket.type == BracketType.CLOSE:
+            if len(stack) == 0:
+                return 0
+            last_bracket: Bracket = stack.pop()
+            if last_bracket.name is not bracket.name:
+                return bracket.score
+        else:
+            stack.append(bracket)
+
+    return 0
+
+def incomplete_bracket_pricer(line: str) -> int:
+    if not line:
+        return 0
+    stack = deque()
+    for c in line:
+        bracket = create_bracket(c, score_dic=get_scoring_2())
+        if bracket.type == BracketType.CLOSE:
+            if not stack:
+                return 0
+            last_bracket: Bracket = stack.pop()
+            if last_bracket.name is not bracket.name:
+                return 0
+        else:
+            stack.append(bracket)
+
+    # incomplete only
+    res = 0
+    # print([x.name.name for x in list(stack)])
+    while stack:
+        res = res * 5 + stack.pop().score
+
+    return res
+
+def solve_1():
+    with open(input_file, 'r', encoding='utf8') as f:
+        res = sum((corrupted_bracket_pricer(line.strip()) for line in f.readlines()))
+        print(res)
+
+
+def solve_2():
+    with open(input_file, 'r', encoding='utf8') as f:
+        res = sorted(filter(lambda x: x > 0, 
+                            (incomplete_bracket_pricer(line.strip()) for line in f.readlines())))
+        print(res[len(res)//2])
+
+
+if __name__ == '__main__':
+    solve_1()
+    solve_2()
+    lp = LineProfiler()
+    lp.add_function(incomplete_bracket_pricer)
+    # lp.add_function(create_bracket)
+    lp_wrapper = lp(solve_2)
+    lp_wrapper()
+    lp.print_stats()
--- aoc2021/src/t3/task.py
+++ aoc2021/src/t3/task.py
@@ -19,26 +19,27 @@ def solve_1():
                     zeros[idx] += 1
                 else:
                     ones[idx] += 1
-        
+
         gamma = ['0' if ones[i] < zeros[i] else '1' for i in range(len(ones))]
         epsilon = ['0' if gamma[i] == '1' else '1' for i in range(len(gamma))]
 
         gamma = int(''.join(gamma),base=2)
         epsilon = int(''.join(epsilon),base=2)
-            
+
         print(f'{gamma =}, {epsilon =}')
         print(gamma * epsilon)
 
 def solve_2():
-     with open(input_file, 'rt', encoding='utf8') as f:
-        lines = []
-        [lines.append(line.strip()) for line in f]
+    with open(input_file, 'rt', encoding='utf8') as f: 
+        lines = [line.strip() for line in f]
 
         def get_oxygen_bit(idx: int, bits: List[str]):
-            return '1' if len(list(filter(lambda x: x[idx] == '1', bits))) >= len(list(filter(lambda x: x[idx] == '0', bits))) else '0'
+            return '1' if len(list(filter(lambda x: x[idx] == '1', bits))) >= len(
+                list(filter(lambda x: x[idx] == '0', bits))) else '0'
 
         def get_co2_bit(idx: int, bits: List[str]):
-            return '0' if len(list(filter(lambda x: x[idx] == '0', bits))) <= len(list(filter(lambda x: x[idx] == '1', bits))) else '1'
+            return '0' if len(list(filter(lambda x: x[idx] == '0', bits))) <= len(
+                list(filter(lambda x: x[idx] == '1', bits))) else '1'
 
         oxygen = lines.copy()
         co2 = lines.copy()
@@ -47,7 +48,7 @@ def solve_2():
         while i < n:
             oxygen_bit = get_oxygen_bit(idx=i, bits=oxygen)
             co2_bit = get_co2_bit(idx=i, bits=co2)
-            
+
             if len(oxygen) > 1:
                 oxygen = list(filter(lambda x: x[i] == oxygen_bit, oxygen))
             if len(co2) > 1:
@@ -56,7 +57,7 @@ def solve_2():
 
         oxygen = int(''.join(oxygen),base=2)
         co2 = int(''.join(co2),base=2)
-            
+
         print(f'{oxygen =}, {co2 =}')
         print(oxygen * co2)
 
