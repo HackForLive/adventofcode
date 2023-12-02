ce6196336abef4d04f18e7fe8144c7ccbc81bf62
Michael <michael.hrabe@gmail.com>
2023-12-02 02:57:04 +0100

    hura, slava, vole konecne, t15 2021

--- aoc2021/src/t15/task.py
+++ aoc2021/src/t15/task.py
@@ -3,16 +3,15 @@ import time
 import os
 import functools
 import pathlib
-from typing import List, Tuple
-
-from collections import deque
-from line_profiler import LineProfiler
-import numpy as np
 import heapq
+from typing import List, Tuple
+from line_profiler import LineProfiler
+
+import numpy as np
 
 
 curr_dir = pathlib.Path(__file__).parent.resolve()
-input_file = os.path.join(curr_dir, 'input_test.txt')
+input_file = os.path.join(curr_dir, 'test.txt')
 matrix_file = os.path.join(curr_dir, 'matrix.txt')
 
 
@@ -51,33 +50,20 @@ def get_matrix_with_offset(matrix: np.matrix, val: int, offset: int)  -> np.matr
 
 def bfs(matrix: np.matrix, offset: int, shape: Tuple[int, int]):
     start = Node(x=0 + offset, y = 0 + offset, value=matrix[0 + offset,0 + offset], path=[])
-    # q = deque()
     q = []
-    # q.append(start)
     heapq.heappush(q, start)
-    # smallest = heapq.heappop(q)
 
     n_max = 100000000
 
     closest_m = np.zeros(shape=matrix.shape, dtype=int) + n_max
     closest_m[start.y, start.x] = 0
 
-    final: Node = None
-
     while q:
-        # curr = q.pop()
         curr = heapq.heappop(q)
 
-        # print(closest_m)
-        # if closest_m[curr.y, curr.x] > 100*2*10:
-        #     continue
-
         if curr.x == shape[0] and curr.y == shape[1]:
-            final = curr
             n_max = closest_m[shape[1], shape[0]]
-            # print(n_max)
-        #     print(curr)
-        # print(curr)
+
         for direction in ([-1, 0], [1, 0], [0, 1], [0, -1]):
             x = curr.x + direction[1]
             y = curr.y + direction[0]
@@ -107,23 +93,29 @@ def solve_2():
     offset = 1
     val = -1
     extend_val = 5
-    mod_val = 10
+    # modulo fix = 10 - 1
+    mod_val = 9
     with open(input_file, 'r', encoding='utf8') as f:
         arr_2d = np.array([[int(number) for number in line.strip()] for line in f.readlines()])
         matrix = np.asmatrix(arr_2d)
         height = matrix.shape[0]
         width = matrix.shape[1]
+        # modulo fix
+        matrix = matrix - 1
         matrix_extended = np.tile(matrix,(extend_val,extend_val))
         for i in range(0,extend_val):
             for j in range(0,extend_val):
-                matrix_extended[i*height:(i+1)*height,j*width:(j+1)*width] = (matrix + matrix*(j+i))%mod_val
+                matrix_extended[i*height:(i+1)*height,j*width:(j+1)*width] = (
+                    matrix + (j+i))%mod_val
+
+        matrix_extended = matrix_extended + 1
 
         with open(matrix_file, mode='w', encoding='utf-8') as f:
             np.savetxt(f, matrix_extended, fmt='%.0f', delimiter=',')
         # matrix_extended.tofile(matrix_file, sep='\r\n')
 
         matrix_with_offset = get_matrix_with_offset(matrix=matrix_extended, val=val, offset=offset)
-        print(matrix_with_offset)
+        # print(matrix_with_offset)
         res = bfs(matrix=matrix_with_offset, offset=offset, shape=matrix_extended.shape)
         print(res)
 
--- aoc2021/src/t15/test.py
+++ aoc2021/src/t15/test.py
@@ -8,11 +8,17 @@ mt_file = os.path.join(curr_dir, 'mt.txt')
 
 A = np.matrix([[1, 1], [1, 1]])
 
+# to modulo properly
+A = A - 1
+
 E = np.tile(A,(5,5))
 for i in range(0,5):
     for j in range(0,5):
-        E[i*2:(i+1)*2,j*2:(j+1)*2] = (A + A*(j+i))%4
-        print(E)
+        E[i*2:(i+1)*2,j*2:(j+1)*2] = (A + (j+i))%4
+        
+
+E = E + 1
+print(E)
 
 with open(mt_file, mode='w', encoding='utf-8') as f:
     np.savetxt(f, E, fmt='%.0f', delimiter=',')
