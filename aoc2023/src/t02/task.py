e3e7c69af9311a5ff28e6edb116e4377b9217339
Michael <michael.hrabe@gmail.com>
2023-12-02 12:33:35 +0100

    t02 2023 part two

--- aoc2023/src/t02/task.py
+++ aoc2023/src/t02/task.py
@@ -1,6 +1,8 @@
 import os
 import pathlib
 
+import numpy as np
+
 curr_dir = pathlib.Path(__file__).parent.resolve()
 input_file = os.path.join(curr_dir, 'test.txt')
 
@@ -24,16 +26,13 @@ def solve_1():
             l = line.strip()
             parts = l.split(':')
             game_id = parts[0].strip().split(' ')[1]
-            print(game_id)
             bags = parts[1].strip().split(';')
 
             is_ok = True
             for part in bags:
                 p = part.strip().split(',')
-                # print(p)
                 for i_b in p:
                     item = i_b.strip().split(' ')
-                    print(f"{item[0] =}, {item[1] =}")
                     if not check(name=item[1], val=int(item[0])):
                         is_ok = False
                         break
@@ -43,5 +42,30 @@ def solve_1():
                 res_sum += int(game_id)
         print(res_sum)
 
+# @timer_decorator
+def solve_2():
+    with open(input_file, 'r', encoding='utf8') as f:
+        res_sum = 0
+        for line in f.readlines():
+            l = line.strip()
+            parts = l.split(':')
+            bags = parts[1].strip().split(';')
+
+            dict_res = {}
+            for part in bags:
+                p = part.strip().split(',')
+                for i_b in p:
+                    item = i_b.strip().split(' ')
+                    name=item[1]
+                    val=int(item[0])
+                    if name in dict_res:
+                        if dict_res[name] < val:
+                            dict_res[name] = val
+                    else:
+                        dict_res[name] = val
+            res_sum += np.prod(list(dict_res.values()))
+        print(res_sum)
+
 if __name__ == '__main__':
     solve_1()
+    solve_2()
