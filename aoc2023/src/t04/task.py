85555ed2e5f9d7a183b13486257d905d7882b07d
Michael <michael.hrabe@gmail.com>
2023-12-05 16:01:10 +0100

    t04 2023

--- aoc2023/src/t04/task.py
+++ aoc2023/src/t04/task.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 
+import numpy as np
 
 curr_dir = pathlib.Path(__file__).parent.resolve()
 input_file = os.path.join(curr_dir, 'test.txt')
@@ -22,5 +23,28 @@ def solve_1():
     print(res)
 
 
+# @timer_decorator
+def solve_2():
+    res = []
+    with open(input_file, 'r', encoding='utf8') as f:
+        for line in f:
+            l = line.strip()
+            parts = l.split(':')
+            numb = parts[1].strip().split('|')
+
+            winning = set([int(n) for n in numb[0].strip().split(' ') if n.isdigit()])
+            curr = [int(n) for n in numb[1].strip().split(' ') if n.isdigit()]
+
+            res.append(len(winning.intersection(curr)))
+    n = len(res)
+    ones = np.ones(n, dtype=int)
+
+    for rid, r in enumerate(res):
+        ones[rid+1:min(rid+1+r, n)] += ones[rid]
+
+    print(sum(ones))
+
+
 if __name__ == '__main__':
     solve_1()
+    solve_2()
