7748c479871df4d785a6fae0ca4e772252c03048
Michael <michael.hrabe@gmail.com>
2023-12-06 11:28:46 +0100

    t06 2023 part 2

--- aoc2023/src/t06/task.py
+++ aoc2023/src/t06/task.py
@@ -5,7 +5,26 @@ curr_dir = pathlib.Path(__file__).parent.resolve()
 input_file = os.path.join(curr_dir, 'test.txt')
 
 
-def parse():
+def parse_2():
+
+    with open(input_file, 'r', encoding='utf8') as f:
+        lines = f.readlines()
+
+        idl = 0
+        times = []
+        distance = []
+        while idl < len(lines):
+            if 'Time:' in lines[idl]:
+                times = [n.strip() for n in lines[idl].split(' ')[1:] if n.strip().isdigit()]
+            elif 'Distance:' in lines[idl]:
+                distance = [n.strip() for n in lines[idl].split(' ')[1:]
+                            if n.strip().isdigit()]
+            idl = idl + 1
+
+    return int(''.join(times)), int(''.join(distance)) 
+
+
+def parse_1():
 
     with open(input_file, 'r', encoding='utf8') as f:
         lines = f.readlines()
@@ -24,7 +43,7 @@ def parse():
     return list(zip(times, distance))
 
 def solve_1():
-    time_dist = parse()
+    time_dist = parse_1()
     # print(time_dist)
     res_sum = 1
     for t_d in time_dist:
@@ -39,6 +58,19 @@ def solve_1():
         res_sum *= n
     print(res_sum)
 
+def solve_2():
+    time, dist = parse_2()
+    # print(time, dist)
+
+        # try every time, exlude 0 and last
+    n = 0
+    for t in range(1, time, 1):
+        if (time - t)*t > dist:
+            n = n + 1
+    print(n)
+    # 71503
+    # too low
 
 if __name__ == '__main__':
     solve_1()
+    solve_2()
