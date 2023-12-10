0d8dbbf2765070528abb0f5a5cea75f2f32f1a20
Michael <michael.hrabe@gmail.com>
2023-12-10 21:26:01 +0100

    t10 2023 part 1

--- aoc2023/src/t10/task.py
+++ aoc2023/src/t10/task.py
@@ -2,12 +2,12 @@ from __future__ import annotations
 from collections import deque
 import os
 import pathlib
-from typing import Tuple
+from typing import List, Tuple
 
 import numpy as np
 
 curr_dir = pathlib.Path(__file__).parent.resolve()
-input_file = os.path.join(curr_dir, 'input_test.txt')
+input_file = os.path.join(curr_dir, 'test.txt')
 
 
 def get_matrix_with_offset(matrix: np.matrix, val: str, offset: int)  -> np.matrix:
@@ -29,25 +29,53 @@ def find_start(start: str, matrix: np.matrix):
 
 def bfs(token: str, start_idx: Tuple[int, int], matrix: np.numpy) -> Tuple[bool, int]:
     is_cycle = False
-    cycle_len = -1
+    cycle_len = 0
+    paths = np.zeros(shape=matrix.shape, dtype=int)
+    # print(f"{start_idx =}, {token =}")
 
+    paths[start_idx] = -1
     stack = deque()
-    stack.append(start_idx)
-
-    while stack:
-        # traverse stack
+    for p in pipe_mapping(pipe=token):
+        # print(f"{p =}")
+        stack.append((p[0] + start_idx[0], p[1] + start_idx[1]))
+        paths[(p[0] + start_idx[0], p[1] + start_idx[1])] = 1
+        # first one
         break
 
+    while stack and not is_cycle:
+        curr = stack.pop()
+        pipe = matrix[curr[0], curr[1]]
+        # print(f"{curr =}, {pipe =}")
+
+        if pipe == 'b' or pipe == '.':
+            continue
+        if pipe == 'S':
+            is_cycle = True
+            break
+
+        for k in pipe_mapping(pipe=pipe):
+            tmp = (k[0] + curr[0], k[1] + curr[1])
+            if cycle_len < 3 and paths[tmp] == -1:
+                continue
+            if paths[tmp] == -1:
+                is_cycle = True
+            if paths[tmp] != 0:
+                continue
+            stack.append(tmp)
+            paths[tmp] = paths[curr] + 1
+        cycle_len = cycle_len + 1
+
+    print(paths)
     return is_cycle, cycle_len
 
-def pipe_mapping(pipe: str):
+def pipe_mapping(pipe: str) -> List[Tuple[int, int]]:
     pipe_dic = {
-        'L' : [[-1, 0], [0, 1]], 
-        '|' : [[1, 0], [-1, 0]],
-        '7': [[1, 0], [0, -1]],
-        '-': [[0, 1], [0, -1]],
-        'F': [[1, 0], [0, 1]],
-        'J': [[-1, 0], [0, -1]]
+        'L' : [(-1, 0), (0, 1)], 
+        '|' : [(1, 0), (-1, 0)],
+        '7': [(1, 0), (0, -1)],
+        '-': [(0, 1), (0, -1)],
+        'F': [(1, 0), (0, 1)],
+        'J': [(-1, 0), (0, -1)]
     }
 
     return pipe_dic[pipe]
@@ -66,6 +94,7 @@ def solve_1():
         is_cycle, length = bfs(token=s, start_idx=start_idx, matrix=matrix)
         if is_cycle:
             print(length)
+            print((length+1)/2)
             break
 
 if __name__ == '__main__':
