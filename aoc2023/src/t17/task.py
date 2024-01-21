adde86820c96e7265d15d0fd1e7e2df41ad8e074
Michael <michael.hrabe@gmail.com>
2024-01-21 19:28:24 +0100

    aoc 17

--- aoc2023/src/t17/task.py
+++ aoc2023/src/t17/task.py
@@ -12,6 +12,9 @@ curr_dir = pathlib.Path(__file__).parent.resolve()
 input_file = os.path.join(curr_dir, 'input_test.txt')
 
 class Direction(Enum):
+    """
+    N/S/E/W
+    """
     NORTH = [-1, 0]
     EAST = [0, 1]
     SOUTH = [1, 0]
@@ -46,25 +49,25 @@ def get_next_right_dir(direction: Direction):
     return directions[direction]
 
 
-class Instruction:
-    def __init__(self, point: Tuple[int, int], direction: Direction, value: int) -> None:
+class Node:
+    prev: Direction
+    count: int
+    def __init__(self, point: Tuple[int, int], direction: Direction, prev: Node) -> None:
         self.point = point
         self.direction = direction
-        self.value = value
-        self.path_history: List[Direction] = []
+        self.prev = prev
 
-    def __eq__(self, obj):
-        return isinstance(obj, Instruction) and (obj.point == self.point
-                                                 and obj.direction == self.direction)
 
-    def __hash__(self):
-        return hash((self.point, self.direction.name))
+    # def __eq__(self, obj):
+    #     return isinstance(obj, Node) and (obj.point == self.point
+    #                                              and obj.direction == self.direction
+    #                                              and obj.value == self.value)
 
-    def __lt__(self, other):
-        return self.value < other.value
+    # def __hash__(self):
+    #     return hash((self.point, self.direction.name, self.value))
 
-    def __repr__(self):
-        return f"Instruction(p={self.point}, dir={self.direction})"
+    # def __lt__(self, other):
+    #     return self.value < other.value
 
 
 def get_matrix_with_offset(matrix: np.matrix, val: int, offset: int)  -> np.matrix:
@@ -80,77 +83,80 @@ def parse():
         matrix = np.asmatrix(arr_2d)
         return matrix
 
-def bfs_with_weights(matrix: np.matrix, start_instr: Instruction, shape):
-    # TODO: improve with Dijkstra algo
-    q = []
-    heapq.heappush(q, start_instr)
 
-    n_max = 999
+def get_number_from_direction(d: Direction):
+    if d == Direction.EAST:
+        return 0
+    if d == Direction.NORTH:
+        return 1
+    if d == Direction.WEST:
+        return 2
+    if d == Direction.SOUTH:
+        return 3
+    raise ValueError('Unexpected')
 
-    closest_m = np.zeros(shape=matrix.shape, dtype=int) + n_max
 
-    closest_m[start_instr.point] = 0
-    dict_closest = {
-        Direction.EAST: deepcopy(closest_m),
-        Direction.WEST: deepcopy(closest_m),
-        Direction.SOUTH: deepcopy(closest_m),
-        Direction.NORTH: deepcopy(closest_m),
-    }
+def get_label(n: int, node: Node, offset: int):
+    return (node.point[0]-offset)*n + (node.point[1]-offset)
 
-    while q:
-        curr: Instruction = heapq.heappop(q)
 
-        if curr.point[0] == shape[0] and curr.point[1] == shape[1]:
-            print(curr.path_history)
-            print('---------------------------')
-            n_max = closest_m[shape[0], shape[1]]
-        
-        # if curr.value > n_max:
-        #     continue
+def shortest_path(m: np.matrix, src: Node):
+    # Create a priority queue to store vertices that
+    # are being preprocessed
+    pq = []
+    heapq.heappush(pq, (0, src))
 
-        for direction in [curr.direction, get_next_left_dir(curr.direction),
-                          get_next_right_dir(curr.direction)]:
-            if len(curr.path_history) >= 3 and (
-                curr.path_history[-1] == curr.path_history[-2]) and (
-                    curr.path_history[-2] == curr.path_history[-3]
-                ):
-                if direction == curr.path_history[-1]:
-                    continue
+    # distance map
+    dist = np.ones(shape=m.shape, dtype=float) * float('inf')
 
-            x = curr.point[1] + direction.value[1]
-            y = curr.point[0] + direction.value[0]
-            val = matrix[y, x]
+    # distances as infinite (INF)
+    # dist = [float('inf')] * n
+    # previous node
+    # prev = [float('inf')] * n
 
-            new_instr = Instruction(point=(y,x), direction=direction, value=val + curr.value)
-            new_instr.path_history = deepcopy(curr.path_history)
-            new_instr.path_history.append(curr.direction)
+    dist[src.point] = 0
 
+    while pq:
+        # The first vertex in pair is the minimum distance
+        # vertex, extract it from priority queue.
+        # vertex label is stored in second of pair
+        d, u = heapq.heappop(pq)
+
+        # 'i' is used to get all adjacent vertices of a
+        # vertex
+        for c_dir in [u.direction, get_next_left_dir(u.direction),
+                          get_next_right_dir(u.direction)]:
+
+            y = u.point[0] + c_dir.value[0]
+            x = u.point[1] + c_dir.value[1]
+            p = (y, x)
+            weight = m[p]
             # border
-            if val == -1:
+            if weight == -1:
                 continue
-            if dict_closest[direction][y, x] <= val + dict_closest[direction][curr.point]:
-                continue
-            dict_closest[direction][y, x] = val + dict_closest[direction][curr.point]
-            # q.append(Node(x=x, y=y, value=val, path=curr.path + [(y,x)]))
-            heapq.heappush(q, new_instr)
-       
-    # print(matrix)
-    # print(closest_m)
-    print(dict_closest[Direction.EAST])
-    print(dict_closest[Direction.WEST])
-    print(dict_closest[Direction.NORTH])
-    print(dict_closest[Direction.SOUTH])
-    return closest_m[shape[0], shape[1]]
+
+            # If there is shorted path to v through u.
+            if dist[p] > dist[u.point] + weight:
+                # Updating distance of v
+                dist[p] = dist[u.point] + weight
+                # prev[p] = u
+                heapq.heappush(pq, (dist[p], Node(point=p, direction=c_dir, prev=u)))
+
+    # Print shortest distances stored in dist[]
+    print(dist)
+    print(dist[-2,-2])
+
+
 
 def solve_1():
     matrix = parse()
     offset = 1
     border = -1
     m_offset = get_matrix_with_offset(matrix=matrix, val=border, offset=offset)
-    start_pos = (offset,offset)
-    start_instr = Instruction(point=start_pos, direction=Direction.EAST, value=m_offset[start_pos])
-    start_instr.path_history = []
-    print(bfs_with_weights(matrix=m_offset, start_instr=start_instr, shape=matrix.shape))
+    start_pos = (offset, offset)
+    print(start_pos)
+    src_node = Node(point=start_pos, direction=Direction.EAST, prev=None)
+    shortest_path(m=m_offset, src=src_node)
 
 
 if __name__ == '__main__':
