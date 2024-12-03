fa35b8fde61b21d342abdda5fc69c28215cbe138
HackForLive <michael.hrabe@gmail.com>
2024-12-03 14:50:55 +0100

    aoc2024 t3

--- aoc2024/src/t03/task.py
+++ aoc2024/src/t03/task.py
@@ -9,83 +9,6 @@ t_f = curr_dir / 'test.txt'
 in_f = curr_dir / 'in.txt'
 
 
-def get_pair_multiplication(content: str, idx: int) -> int | None:
-    i = idx
-    if content[idx] != '(':
-        return None
-
-    n1 = []
-    n2 = []
-    sep = False
-
-    while i < len(content):
-        i += 1
-        if content[i] == ',':
-            if sep:
-                return None
-            sep = True
-        elif content[i].isdigit():
-            if sep:
-                n2.append(content[i])
-            else:
-                n1.append(content[i])
-
-        elif content[i] == ')':
-            if n1 and n2:
-                return int(''.join(n1))*int(''.join(n2))
-            else:
-                return None
-        else:
-            return None
-        
-    return None
-        
-
-
-def parse(content: str):
-    i = 0
-    res = 0
-    while i < len(content) - 4:
-        if content[i:i+3] == 'mul':
-            i = i+3
-            tmp = get_pair_multiplication(content=content, idx=i)
-            if tmp is not None:
-                res += tmp
-        else:
-            i += 1
-    return res
-
-
-def parse_2(content: str):
-    i = 0
-    res = 0
-    enable: bool = True
-    while i < len(content) - 4:
-        if content[i:i+3] == 'mul':
-            i = i+3
-            if not enable:
-                continue 
-            tmp = get_pair_multiplication(content=content, idx=i)
-            if tmp is not None:
-                res += tmp
-        elif (i+5 < len(content)) and (content[i:i+4] == "do()"):
-            enable = True
-            i += 4
-        elif (i+8 < len(content)) and (content[i:i+7] == "don't()"):
-            enable = False
-            i += 7
-        else:
-            i += 1
-    return res
-
-
-@timer_decorator
-def solve_1(p: Path):
-    with open(p, encoding='utf-8', mode='r') as file:
-        data = file.read().rstrip()
-    return parse(content=data)
-
-
 @timer_decorator
 def solve_1_regex(p: Path):
     with open(p, encoding='utf-8', mode='r') as file:
@@ -97,10 +20,31 @@ def solve_1_regex(p: Path):
 
 
 @timer_decorator
-def solve_2(p: Path):
+def solve_2_regex(p: Path):
     with open(p, encoding='utf-8', mode='r') as file:
         data = file.read().rstrip()
-    return parse_2(content=data)
+
+        matches = [(m.start(0), m.groups()) for m in re.finditer('mul\\((\\d+),(\\d+)\\)', data)]
+        n_ids = [(m.start(0)) for m in re.finditer('don\'t\\(\\)', data)]
+        y_ids = [0] + [(m.start(0)) for m in re.finditer('do\\(\\)', data)]
+
+
+        res = 0
+        y_id = 0
+        n_id = 0
+        for m in matches:
+            curr_i = m[0]            
+            for i in range(y_id, len(y_ids)):
+                if y_ids[i] > curr_i:
+                    break
+                y_id = i
+            for j in range(n_id, len(n_ids)):
+                if n_ids[j] > curr_i:
+                    break
+                n_id = j
+            if (y_ids[y_id] < curr_i) and (n_ids[n_id] > curr_i) or (n_ids[n_id] < y_ids[y_id]):
+                res += int(m[1][0])*int(m[1][1])
+        return res
 
 
 if __name__ == '__main__':
@@ -113,12 +57,12 @@ if __name__ == '__main__':
     if f_o != 173529487:
         raise ValueError('The first task failed!')
     
-    test_o = solve_2(p=t_f)
+    test_o = solve_2_regex(p=t_f)
     print(test_o)
     if test_o != 48:
         raise ValueError('Test failed!')
     
-    s_o = solve_2(p=in_f)
+    s_o = solve_2_regex(p=in_f)
     if s_o != 99532691:
         raise ValueError('The second task failed!')
 
