71acffd0689094d6533b6946ba415a4638d43c3d
HackForLive <michael.hrabe@gmail.com>
2024-12-03 20:58:15 +0100

    aoc2024 t3

--- aoc2024/src/t03/task.py
+++ aoc2024/src/t03/task.py
@@ -18,32 +18,20 @@ def solve_1_regex(p: Path):
         return functools.reduce(lambda a, b: a + b, 
                                 (int(i[0])*int(i[1]) for i in rgx.findall(data)))
 
-
 @timer_decorator
 def solve_2_regex(p: Path):
     with open(p, encoding='utf-8', mode='r') as file:
         data = file.read().rstrip()
 
-        matches = [(m.start(0), m.groups()) for m in re.finditer('mul\\((\\d+),(\\d+)\\)', data)]
-        n_ids = [(m.start(0)) for m in re.finditer('don\'t\\(\\)', data)]
-        y_ids = [0] + [(m.start(0)) for m in re.finditer('do\\(\\)', data)]
-
-
         res = 0
-        y_id = 0
-        n_id = 0
-        for m in matches:
-            curr_i = m[0]            
-            for i in range(y_id, len(y_ids)):
-                if y_ids[i] > curr_i:
-                    break
-                y_id = i
-            for j in range(n_id, len(n_ids)):
-                if n_ids[j] > curr_i:
-                    break
-                n_id = j
-            if (y_ids[y_id] < curr_i) and (n_ids[n_id] > curr_i) or (n_ids[n_id] < y_ids[y_id]):
-                res += int(m[1][0])*int(m[1][1])
+        enabled = True
+        for m in re.findall('(do\\(\\))|(don\'t\\(\\))|mul\\((\\d+),(\\d+)\\)', data):
+            if m[0] == 'do()':
+                enabled = True
+            elif m[1] == 'don\'t()':
+                enabled = False
+            else:
+                res = res + int(m[2])*int(m[3]) if enabled else res
         return res
 
 
@@ -58,7 +46,6 @@ if __name__ == '__main__':
         raise ValueError('The first task failed!')
     
     test_o = solve_2_regex(p=t_f)
-    print(test_o)
     if test_o != 48:
         raise ValueError('Test failed!')
     
