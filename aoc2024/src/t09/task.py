38a980d8943ead84a5eacbbc59723a032a2500b2
HackForLive <michael.hrabe@gmail.com>
2024-12-09 12:38:31 +0100

    aoc2024 t9

--- aoc2024/src/t09/task.py
+++ aoc2024/src/t09/task.py
@@ -29,11 +29,6 @@ def get_file_system_check_after_move(line: str) -> int:
         else:
             dll.append(Block(length=int(c), allocated=False, identifier=0))
 
-    # while dll:
-    #     curr: Block = dll.popleft()
-    #     print(curr)
-
-    # return 0
     while dll:
         # left one
         curr: Block = dll.popleft()
@@ -66,7 +61,56 @@ def get_file_system_check_after_move(line: str) -> int:
             k += 1
 
     return res
-                    
+
+
+def get_file_system_check_after_full_block_move(line: str) -> int:
+    
+    dll = deque()
+    dll_final = deque()
+    
+    ii = 0
+    for i, c in enumerate(line):
+        if i % 2 == 0:
+            dll.append(Block(length=int(c), allocated=True, identifier=ii))
+            ii += 1
+        else:
+            dll.append(Block(length=int(c), allocated=False, identifier=0))
+
+    while dll:
+        # left one
+        curr: Block = dll.popleft()
+        
+        if curr.allocated:
+            dll_final.append(curr)
+        else:
+            # empty
+            size = curr.length
+            tmp = deque()
+            is_allocated = False
+            while size > 0 and dll:
+                curr_r : Block = dll.pop()
+                if curr_r.allocated and (size - curr_r.length) >= 0:
+                    dll_final.append(curr_r)
+                    if size - curr_r.length > 0:
+                        dll.appendleft(Block(length=size - curr_r.length, allocated=False, identifier=0))
+                    is_allocated = True
+                    tmp.append(Block(length=curr_r.length, allocated=False, identifier=0))
+                    break
+                tmp.append(curr_r)
+
+            while tmp:
+                dll.append(tmp.pop())
+            if not is_allocated:
+                dll_final.append(curr)
+
+    res = 0
+    k = 0
+    while dll_final:
+        curr: Block = dll_final.popleft()
+        for i in range(curr.length):            
+            res += curr.identifier * k
+            k += 1
+    return res
 
 
 @timer_decorator
@@ -74,17 +118,19 @@ def solve_1(p: Path) -> int:
     with open(p, encoding='utf-8', mode='r') as f:
         for line in f:
             return get_file_system_check_after_move(line=line.strip())
+        return 0
     
 @timer_decorator
 def solve_2(p: Path) -> int:
     with open(p, encoding='utf-8', mode='r') as f:
         for line in f:
-            return get_file_system_check_after_move(line=line.strip())
+            return get_file_system_check_after_full_block_move(line=line.strip())
+        return 0
  
 
 if __name__ == '__main__':
     assert solve_1(p=t_f) == 1928
     assert solve_1(p=in_f) == 6356833654075
-    # assert solve_2(p=t_f) == 11387
-    # assert solve_2(p=in_f) == 59002246504791
+    assert solve_2(p=t_f) == 2858
+    assert solve_2(p=in_f) == 6389911791746
     print("All passed!")
