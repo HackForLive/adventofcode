16a954b531ef08926fa03631ef06c8f5765d673a
HackForLive <michael.hrabe@gmail.com>
2024-12-31 14:22:21 +0100

    aoc2024 t23

--- aoc2024/src/t23/task.py
+++ aoc2024/src/t23/task.py
@@ -1,6 +1,8 @@
+from __future__ import annotations
 from typing import List, Dict, Set, Tuple
 from pathlib import Path
 from collections import deque
+from dataclasses import dataclass
 
 from aoc.performance import timer_decorator
 
@@ -8,6 +10,19 @@ curr_dir = Path(__file__).parent
  
 t_f = curr_dir / 'test.txt'
 in_f = curr_dir / 'in.txt'
+
+
+@dataclass(frozen=False, init=True, unsafe_hash=False)
+class Node:
+    name: str
+    parent: Node | None
+
+    def __eq__(self, obj):
+        return isinstance(obj, Node) and obj.name == self.name
+    
+    def __hash__(self):
+        return hash(self.name)
+
 
 def parse(p: Path) -> Tuple[Set, Dict]:
     adj = {}
@@ -45,12 +60,72 @@ def solve(p: Path) -> int:
                 
     return len(res)
 
-def solve_2(p: Path) -> int:
+def find_cycle(start: str, adj: Dict[str, str]) -> Tuple:
+    
+    visited = set()
+
+    s_node = Node(name=start, parent=None)
+    stack = deque([s_node])
+
+    while stack:
+        curr = stack.pop()
+
+        if curr in visited:
+            # get all node names
+
+            res = set([curr.name])
+            tmp = curr
+            while tmp.parent:
+                # if tmp.parent.name in res:
+                #     break
+                res.add(tmp.parent.name)
+                tmp = tmp.parent
+            
+            return tuple(res)
+        visited.add(curr)
+
+        for neighbor in adj[curr.name]:
+            stack.append(Node(name=neighbor, parent=curr))
+
+    return ()
+
+
+def is_complete_graph(v: Tuple, adj: Dict[str, str]):
+    for i in range(len(v)):
+        for j in range(i+1, len(v)):
+            if not v[i] in adj[v[j]]:
+                return False
+    return True
+
+
+def longest_cycle(v: Set[str], adj: Dict[str, str]) -> str:
+    
+    # keep track of cycles
+    # tuple of nodes
+    cycles = set()
+   
+    # for each vertice
+    # do BFS
+    for n in v:
+        cycle = find_cycle(start=n, adj=adj)
+        print(cycle)
+        if is_complete_graph(v=cycle, adj=adj):
+            cycles.add(cycle)
+
+    max_l = 0
+    max_nodes = ()
+    for c in cycles:
+        if len(c) > max_l:
+            max_l = len(c)
+            max_nodes = c
+
+    return ','.join(sorted(set(max_nodes)))
+    
+
+def solve_2(p: Path) -> str:
     v, adj = parse(p=p)
-    # print(v)
-    print(len(v))
-    res =0
-    return res
+    # print(adj)
+    return longest_cycle(v=v, adj=adj)
    
 
 if __name__ == '__main__':
