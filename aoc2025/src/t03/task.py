cfbbab305de0d471a0aa58803af2dc494633de7d
HackForLive <michael.hrabe@gmail.com>
2025-12-03 10:31:19 +0100

    t03

--- aoc2025/src/t03/task.py
+++ aoc2025/src/t03/task.py
@@ -7,13 +7,13 @@ curr_dir = Path(__file__).parent
 t_f = curr_dir / 'test.txt'
 in_f = curr_dir / 'in.txt'
 
-def get_largest_number(bank: list[int], n: int) -> int:
+def get_largest_number(bank: str, n: int) -> int:
     # n - length of number
     ll = len(bank)
     if ll < n:
         raise ValueError(f'n:{n} cannot be made from sequence with less length than n!')
     if ll == n:
-        return int(''.join([str(i) for i in bank]))
+        return int(bank)
     
     num = []
     start = 0
@@ -37,13 +37,13 @@ def get_largest_number(bank: list[int], n: int) -> int:
 def solve_1(p: Path):
     with open(p, 'r', encoding='utf8') as f:
         return functools.reduce(
-            lambda a, b: a + b, [get_largest_number(bank=[int(i) for i in l.strip()], n=2) for l in f])
+            lambda a, b: a + b, [get_largest_number(bank=l.strip(), n=2) for l in f])
 
 @timer_decorator
 def solve_2(p: Path):
     with open(p, 'r', encoding='utf8') as f:
         return functools.reduce(
-            lambda a, b: a + b, [get_largest_number(bank=[int(i) for i in l.strip()], n=12) for l in f])
+            lambda a, b: a + b, [get_largest_number(bank=l.strip(), n=12) for l in f])
 
 if __name__ == '__main__':
     assert solve_1(p=t_f) == 357
