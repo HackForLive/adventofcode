672528077eb505d90aa4f1e97e7ab83346f90553
HackForLive <michael.hrabe@gmail.com>
2025-12-05 23:14:25 +0100

    t05

--- aoc2025/src/t05/task.py
+++ aoc2025/src/t05/task.py
@@ -16,6 +16,30 @@ def get_fresh(ingred: list[int], ranges: list[tuple[int,int]]) -> int:
                 break
     return res
 
+def get_all_fresh_unions(ranges: list[tuple[int,int]]) -> int:
+    # union find
+    s = sorted(ranges, key=lambda k: k[0])
+
+    res = []
+
+    curr = s[0]
+    for i in range(len(s)):
+        if s[i] == curr:
+            continue
+        if curr[1] > s[i][0]:
+            n = (curr[0], max(curr[1], s[i][1]))
+            curr = n
+        else:
+            res.append(curr)
+            curr = s[i]
+    if curr:
+        res.append(curr)
+    
+    c = 0
+    for i in res:
+        c += i[1]-i[0]+1
+    return c
+
 
 @timer_decorator
 def solve_1(p: Path):
@@ -37,10 +61,31 @@ def solve_1(p: Path):
     
     return get_fresh(ingred=ingred, ranges=ranges)
 
+
+@timer_decorator
+def solve_2(p: Path):
+    ranges = []
+    ingred = []
+    is_ingred = False
+    with open(p, 'r', encoding='utf8') as f:
+        for l in f:
+            line = l.strip()
+            if not line:
+                is_ingred = True
+                continue
+
+            if is_ingred:
+                ingred.append(int(line))
+            else:
+                l,r = line.split('-')
+                ranges.append((int(l), int(r)))
+    
+    return get_all_fresh_unions(ranges=ranges)
+
 if __name__ == '__main__':
     assert solve_1(p=t_f) == 3
-    print(solve_1(p=in_f))
+    print(solve_1(p=in_f)) # 520
     
-    # assert solve_2(p=t_f) == 6
-    # assert solve_2(p=in_f) == 6860
+    assert solve_2(p=t_f) == 14
+    print(solve_2(p=in_f)) # too high 347338785050532
     print("All passed!")
