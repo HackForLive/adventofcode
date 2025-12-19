63b6f672534503b08862b670922f0437bf358a69
HackForLive <michael.hrabe@gmail.com>
2025-12-19 22:08:00 +0100

    t12

--- aoc2025/src/t12/task.py
+++ aoc2025/src/t12/task.py
@@ -1,9 +1,6 @@
-from collections import deque
+
 import dataclasses
 from pathlib import Path
-import sys
-
-import numpy as np
 
 from aoc.performance import timer_decorator
 
@@ -17,88 +14,80 @@ class Board:
     long: int
     indices: list[int]
 
-
-def rotate(shape):
+def rotate(shape) -> list[tuple[int, int]]:
     return [(y, 2 - x) for x, y in shape]
 
-def normalize(shape):
-    minx = min(x for x, y in shape)
-    miny = min(y for x, y in shape)
-    return sorted((x - minx, y - miny) for x, y in shape)
+def flip(shape) -> list[tuple[int, int]]:
+    return [(2 - x, y) for x, y in shape]   # horizontal flip
 
-def unique_rotations(shape):
-    rots = set()
+def all_orientations(shape):
+    seen = set()
+    shapes = []
+
     cur = shape
     for _ in range(4):
         cur = rotate(cur)
-        rots.add(tuple(normalize(cur)))
-    return [list(r) for r in rots]
+        for s in (cur, flip(cur)):
+            n = tuple(sorted(s, key=lambda x: (x[0], x[1])))
+            if n not in seen:
+                seen.add(n)
+                shapes.append(list(n))
+    return shapes
 
-# def best_combination():
-#     for each x, y shape 
-
-
-def compute(presents: dict[str, list[tuple[int,int]]], wide: int, long: int, indices: list[int]) -> int:
-
-    # 3x3
-
-    # meta shapes better than 6x3 6x6 etc
+def compute(presents: dict[str, list[tuple[int,int]]], wide: int, long: int, indices: list[int], 
+            max_l: int, max_w: int) -> int:
 
     # naive put all 3x3
     alls = sum(indices)
 
-    upper_bound = (wide // 3)*(long // 3)
-    if upper_bound > alls:
+    upper_bound = wide*long
+    # 3x3
+    if upper_bound >= alls*max_l*max_w: 
         return 1
-
-    print(presents)
-    # print(boards)
     return 0
 
-def compute_all(presents: dict[str, list[tuple[int,int]]], boards: list[Board]) -> int:
-    return sum(compute(presents=presents, wide=b.wide, long=b.long, indices=b.indices) for b in boards)
+def compute_all(presents: dict[str, list[tuple[int,int]]], boards: list[Board], max_l: int, max_w: int) -> int:
+    return sum(compute(presents=presents, wide=b.wide, long=b.long, indices=b.indices, max_l=max_l, 
+                       max_w=max_w) for b in boards)
 
 @timer_decorator
 def solve_1(p: Path):
     presents = {}
     boards = []
     with open(p, 'r', encoding='utf8') as f:
-        is_present = True
         last_p_id = 0
         last_p = []
         i = 0
+
+        max_l = 0 
+        max_w = 0
         for l in f:
-            if 'x' in l:
-                is_present = False
-            
-            if is_present:
-                line = l.strip()
-                if ':' in line:
-                    last_p_id = int(line[0])
-                elif not line:
-                    presents[last_p_id] = last_p
-                    last_p = []
-                    i = 0
-                    continue
-                else:
-                    for j in range(3):
-                        if line[j] == '#':
-                            last_p.append((i,j))
-                    i += 1
-                    
-            else:
+            line = l.strip()
+
+            if 'x' in line:
                 le, ri = l.strip().split(':')
 
-                wide, long = [int(i.strip()) for i in le.split('x') if i.strip()]
-                indices = [int(i.strip()) for i in ri.split(' ') if i.strip()]
+                wide, long = map(int, le.split('x'))
+                indices = list(map(int, ri.split()))
 
                 boards.append(Board(wide=wide, long=long, indices=indices))
+            if ':' in line:
+                last_p_id = int(line[0])
+                last_p = []
+                i = 0
+            elif not line:
+                presents[last_p_id] = last_p                
+                continue
+            elif any(i in line for i in ('#', '.')):
+                last_p.extend(((i,j) for j in line if j == '#'))
+                i += 1
+                max_w = max(max_w, len(line))
+                max_l = max(max_l, i)
 
-    return compute_all(presents=presents, boards=boards)
+    return compute_all(presents=presents, boards=boards, max_w=max_w, max_l=max_l)
 
 if __name__ == '__main__':
-    print(solve_1(p=t_f))
-    assert solve_1(p=t_f) == 2
-    print(solve_1(p=in_f)) # 607
+    print(solve_1(p=t_f)) # incorrect
+    print(solve_1(p=in_f)) # 591
 
     print("All passed!")
